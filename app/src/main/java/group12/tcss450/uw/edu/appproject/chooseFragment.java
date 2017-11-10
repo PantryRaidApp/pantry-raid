9cdbc2990e22d780cc7022bce81bb0897874145e
samantha <srs39@uw.edu>
2017-11-09 20:39:51 -0800

    set up so when runs will  go to fragment to choose activity (login, register, search recipies), working on setting up buttons

--- app/src/main/java/group12/tcss450/uw/edu/appproject/chooseFragment.java
+++ app/src/main/java/group12/tcss450/uw/edu/appproject/chooseFragment.java
@@ -11,9 +11,9 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class chooseFragment extends Fragment {
+public class chooseFragment extends Fragment implements View.OnClickListener{
 
-
+    private OnFragmentInteractionListener mListener;
     public chooseFragment() {
         // Required empty public constructor
     }
@@ -25,5 +25,24 @@ public class chooseFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_choose, container, false);
     }
+    @Override
+    public void onClick(View view) {
+        if (mListener != null) {
+            switch (view.getId()) {
+                case R.id.loginButton:
+                    mListener.onFragmentInteraction(getString(R.string.login_button));
+                    break;
+                case R.id.regButton:
+                    mListener.onFragmentInteraction(getString(R.string.register_button));
+                    break;
+                case R.id.searchButton:
+                    mListener.onFragmentInteraction(getString(R.string.search_button));
+                    break;
+            }
+        }
+    }
+    public interface OnFragmentInteractionListener {
 
+        void onFragmentInteraction(String theString);
+    }
 }
