446e5144a6e4fcc7da3b03e18a431b90e4e3f2d6
samantha <srs39@uw.edu>
2017-11-09 20:50:35 -0800

    set up so when runs will  go to fragment to choose activity (login, register, search recipies), working on setting up buttons

--- app/src/main/java/group12/tcss450/uw/edu/appproject/chooseFragment.java
+++ app/src/main/java/group12/tcss450/uw/edu/appproject/chooseFragment.java
@@ -1,11 +1,13 @@
 package group12.tcss450.uw.edu.appproject;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -22,9 +24,32 @@ public class chooseFragment extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose, container, false);
+        View v = inflater.inflate(R.layout.fragment_choose, container, false);
+        Button b = (Button) v.findViewById(R.id.loginButton);
+        b.setOnClickListener(this);
+        b = (Button) v.findViewById(R.id.regButton);
+        b.setOnClickListener(this);
+        b= (Button) v.findViewById(R.id.searchButton);
+        b.setOnClickListener(this);
+        return v;
     }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
     @Override
     public void onClick(View view) {
         if (mListener != null) {
