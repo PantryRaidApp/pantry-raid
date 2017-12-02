4a9b094b92f72dd772ad09cd4eebed9ffb76565b
Zira Cook <ziracook@gmail.com>
2017-12-02 02:08:10 -0800

    Added DisplayRecipesFragments changes. Mostly includes foundation work for API calls.
    Added fix so users can't enter an empty ingredient string

--- app/src/main/java/group12/tcss450/uw/edu/appproject/Fragments/DisplayRecipesFragment.java
+++ app/src/main/java/group12/tcss450/uw/edu/appproject/Fragments/DisplayRecipesFragment.java
@@ -7,8 +7,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -19,58 +17,75 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-import group12.tcss450.uw.edu.appproject.API.ApiRecipeResponse;
 import group12.tcss450.uw.edu.appproject.API.ApiRecipe;
-import group12.tcss450.uw.edu.appproject.R;
+import group12.tcss450.uw.edu.appproject.API.ApiRecipeResponse;
 import group12.tcss450.uw.edu.appproject.API.Response;
-
+import group12.tcss450.uw.edu.appproject.R;
 
 /**
- * TODO CLEAN UP
  * A simple {@link Fragment} subclass.
- * Provides functionality to display the top 30 recipes from food2fork.
+ * Use the {@link DisplayRecipesFragment#newInstance} factory method to
+ * create an instance of this fragment.
  */
-public class RecipeViewFragment extends Fragment implements View.OnClickListener{
+public class DisplayRecipesFragment extends Fragment{
+    //Strings used to store data for calling the API
     private String myApiKey;
     private String myApiCall;
 
-    private Button myButton;
-    private TextView myTextView;
-
+    //GSON objects for parsing JSON returned by API call
     private GsonBuilder myBuilder;
     private Gson myGson;
 
-    public RecipeViewFragment() {
+    //String used to identify list parameter
+    private static final String ARG_PARAM = "ingredientList";
+    //List of ingredients used to query API
+    private ArrayList<String> mIngredientList;
+
+
+    public DisplayRecipesFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param ingredientList list of ingredients as strings
+     * @return A new instance of fragment DisplayRecipesFragment.
+     */
+    public static DisplayRecipesFragment newInstance(ArrayList<String> ingredientList) {
+        DisplayRecipesFragment fragment = new DisplayRecipesFragment();
+        Bundle args = new Bundle();
+        args.putStringArrayList(ARG_PARAM, ingredientList);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mIngredientList = getArguments().getStringArrayList(ARG_PARAM);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_display_top_recipes, container, false);
-
+        View v = inflater.inflate(R.layout.fragment_display_recipes, container, false);
         myApiKey = getString(R.string.food2ForkKey);
-        myApiCall = "http://food2fork.com/api/search?key=" + myApiKey + "&sort=t";
-
-        myButton = v.findViewById(R.id.callButton);
-        myButton.setOnClickListener(this);
-        myTextView = v.findViewById(R.id.apiReturnTextView);
-
+        myApiCall = "http://food2fork.com/api/search?key=" + myApiKey + "&q=";
         myBuilder = new GsonBuilder();
         myBuilder.setPrettyPrinting();
         myGson = myBuilder.create();
-
         return v;
     }
 
-    @Override
-    public void onClick(View view) {
-        AsyncTask<String, Void, String> task = new GetWebServiceTask();
-        task.execute(myApiCall);
-    }
+    //http://food2fork.com/api/search?key=e60347278aefc1439128a6281dab7812&q=shredded%20chicken,onion,green%20pepper
 
     /**
      * Handles the webservice methods for a GET call to the api.
@@ -117,7 +132,7 @@ public class RecipeViewFragment extends Fragment implements View.OnClickListener
             Response<ApiRecipe> recipeResponse = myGson.fromJson(result, ApiRecipeResponse.class);
             String recipeString = recipeResponse.getRecipes().toString();
             //recipeString = recipeString.replaceAll(",", "\n");
-            myTextView.setText(recipeString);
         }
     }
+
 }
--- app/src/main/java/group12/tcss450/uw/edu/appproject/Fragments/RecipeSearchFragment.java
+++ app/src/main/java/group12/tcss450/uw/edu/appproject/Fragments/RecipeSearchFragment.java
@@ -5,25 +5,37 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.ListViewCompat;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.List;
 
 import group12.tcss450.uw.edu.appproject.R;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RecipeSearchFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class RecipeSearchFragment extends Fragment implements View.OnClickListener{
     private FavoritesFragment.OnFragmentInteractionListener mListener;
+
+    ArrayList<String> mIngredientList;
+    ArrayAdapter<String> mAdapter;
+    EditText mIngredientSearchBar;
+    ListView mListView;
 
     public RecipeSearchFragment() {
         // Required empty public constructor
@@ -33,20 +45,93 @@ public class RecipeSearchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mIngredientList = new ArrayList<>();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_search, container, false);
+        View v = inflater.inflate(R.layout.fragment_recipe_search, container, false);
+
+        mAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+                R.layout.recipe_ingredient, mIngredientList);
+
+        if (mListView == null) {
+            mListView = (ListView)v.findViewById(R.id.ingredientList);
+        }
+        mListView.setAdapter(mAdapter);
+
+        Button b = v.findViewById(R.id.doneButton);
+        b.setOnClickListener(this);
+
+        b = v.findViewById(R.id.addButton);
+        b.setOnClickListener(this);
+
+        b = v.findViewById(R.id.clearButton);
+        b.setOnClickListener(this);
+
+        mIngredientSearchBar = (EditText)v.findViewById(R.id.searchBar);
+        mIngredientSearchBar.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                Log.d(TAG, "onKey: pressed");
+                //When enter is pressed the current entered text is added to the ingredient list
+                if (keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    if (i == KeyEvent.KEYCODE_ENTER) {
+                        addIngredientToList();
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        });
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.wtf("WTF", "onItemClick worked");
+                String ingredient = mIngredientList.get(i);
+                Log.d(TAG, "onItemClick: remove" + ingredient);
+                removeItemFromIngredientList(ingredient);
+            }
+        });
+
+        return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+    /**
+     * Adds the current text in the search bar to a list of ingredients.
+     */
+    private void addIngredientToList() {
+        String ingredient = mIngredientSearchBar.getText().toString().trim().toLowerCase();
+        if (!mIngredientList.contains(ingredient) && !ingredient.equals("")) {
+            mIngredientList.add(ingredient);
+            mAdapter.notifyDataSetChanged();
+        }
+        mIngredientSearchBar.setText("");
+    }
+
+    /**
+     * Clear all ingredients from the search list.
+     */
+    private void clearIngredientList() {
+        mIngredientList.clear();
+        mAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Removes an item from the backend ingredient list and updates the front end ListView.
+     * @param ingredientToRemove the ingredient to remove.
+     */
+    private void removeItemFromIngredientList(String ingredientToRemove) {
+        if (mIngredientList.contains(ingredientToRemove)) {
+            mIngredientList.remove(ingredientToRemove);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -70,6 +155,22 @@ public class RecipeSearchFragment extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.addButton:
+                addIngredientToList();
+                break;
+            case R.id.doneButton:
+                //TODO send ingredients back to activity
+                //So it goes to DisplayRecipesFragment
+                break;
+            case R.id.clearButton:
+                clearIngredientList();
+                break;
+        }
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -81,7 +182,6 @@ public class RecipeSearchFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 
