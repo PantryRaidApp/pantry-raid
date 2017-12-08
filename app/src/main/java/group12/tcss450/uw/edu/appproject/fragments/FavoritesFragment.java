fd8f8fb9338d8200f9616bbd75f118fd1cff7bcb
Zira Cook <ziracook@gmail.com>
2017-12-08 15:15:09 -0800

    Added onAttach() to FavoritesFragment to stop null pointer exception is trying to call onFragmentInteractionListener

--- app/src/main/java/group12/tcss450/uw/edu/appproject/fragments/FavoritesFragment.java
+++ app/src/main/java/group12/tcss450/uw/edu/appproject/fragments/FavoritesFragment.java
@@ -127,6 +127,23 @@ public class FavoritesFragment extends Fragment implements View.OnClickListener{
     }
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FavoritesFragment.OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
     public void onClick(View v) {
 
     }
